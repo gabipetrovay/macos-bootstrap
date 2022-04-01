c931f33b482922baedaa67eb29d2a1773b330041
Gabriel Petrovay <gabipetrovay@gmail.com>
2022-04-01 11:25:00 +0200

    fixed iterm path in setup script

--- iterm2/setup.sh
+++ iterm2/setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+DIRNAME=`dirname "$0"`
+
 mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
-cp ./profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json
+cp "${DIRNAME}/profiles.json" ~/Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json
