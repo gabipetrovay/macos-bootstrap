bd436f8b0146fea3f03e0b8e1ac20a8225f9d52a
Gabriel Petrovay <gabipetrovay@gmail.com>
2022-04-11 14:07:42 +0200

    Added git personal vs work configuration

--- git/secrets-example.sh
+++ git/secrets-example.sh
@@ -1,2 +1,14 @@
 export GIT_USER_NAME="Your Name"
 export GIT_USER_EMAIL=your.email@example.com
+
+export GIT_WORK_CONFIG_PATH=~/.gitconfig-work
+export GIT_WORK_CONFIG_CONDITION="gitdir:~/**/Work/"
+export GIT_WORK_CONFIG='
+[user]
+	name = Your Name
+	email = your.email@company.com
+
+[author]
+	name = Your Name
+	email = your.email@company.com
+'
--- git/setup.sh
+++ git/setup.sh
@@ -4,3 +4,12 @@ git config --global init.defaultBranch master
 
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
+git config --global author.name "${GIT_USER_NAME}"
+git config --global author.email "${GIT_USER_EMAIL}"
+
+if [ -n "${GIT_WORK_CONFIG}" ]
+then
+  git config --global "includeIf.${GIT_WORK_CONFIG_CONDITION}.path" "${GIT_WORK_CONFIG_PATH}"
+
+  echo "${GIT_WORK_CONFIG}" >> "${GIT_WORK_CONFIG_PATH}"
+fi
