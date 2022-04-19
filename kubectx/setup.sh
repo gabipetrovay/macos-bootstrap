2db85a2e8216c92a8406ae0131546aff898803ee
Gabriel Petrovay <gabipetrovay@gmail.com>
2022-04-19 14:51:13 +0200

    Added kns alias for kubectx installation

--- kubectx/setup.sh
+++ kubectx/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo '' >> ~/.zprofile
-echo '# kubectx alias' >> ~/.zprofile
+echo '# kubectx aliases' >> ~/.zprofile
 echo 'alias kx=kubectx' >> ~/.zprofile
+echo 'alias kns=kubens # not using kn as this is the knative CLI' >> ~/.zprofile
 echo '# kubectx completion' >> ~/.zprofile
 echo 'fpath=($(dirname $(dirname $(which kubectx))/$(readlink $(which kubectx)))/../share/zsh/site-functions $fpath)' >> ~/.zprofile
