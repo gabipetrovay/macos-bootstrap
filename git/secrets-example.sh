337e87e4b9ebed2f79b3b88cc38cd0f0dc96f7d7
Gabriel Petrovay <gabipetrovay@gmail.com>
2022-06-21 14:22:59 +0200

    More personalized git config setup

--- git/secrets-example.sh
+++ git/secrets-example.sh
@@ -3,12 +3,10 @@ export GIT_USER_EMAIL=your.email@example.com
 
 export GIT_WORK_CONFIG_PATH=~/.gitconfig-work
 export GIT_WORK_CONFIG_CONDITION="gitdir:~/**/Work/"
-export GIT_WORK_CONFIG='
-[user]
+export GIT_WORK_CONFIG='[user]
 	name = Your Name
 	email = your.email@company.com
 
 [author]
 	name = Your Name
-	email = your.email@company.com
-'
+	email = your.email@company.com'
--- git/setup.sh
+++ git/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 git config --global init.defaultBranch master
+git config --global pull.rebase false
+git config --global core.pager "less -FX"
 
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
