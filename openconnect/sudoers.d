9915b07e40be050e278f13ede50e56fdee81fbde
Gabriel Petrovay <gabipetrovay@gmail.com>
2023-02-06 18:18:44 +0100

    Fixed sudo rules for openconnect commands

--- openconnect/sudoers.d
+++ openconnect/sudoers.d
@@ -1,9 +1,9 @@
 # do not prompt for sudo password when openconnect is ran with sudo
 # and allow the user to set environment variables when running openconnect
-%admin      ALL=(ALL) NOPASSWD:SETENV: /opt/homebrew/bin/openconnect
+%admin      ALL=(ALL) NOPASSWD:SETENV: /opt/homebrew/bin/openconnect, \
+                /usr/bin/pkill openconnect
 
 # do not prompt for sudo password when other openconnect-related commands are ran
 %admin      ALL=(ALL) NOPASSWD: \
-                /usr/bin/pkill openconnect, \
                 /bin/cp -r /Users/*/.vpn-forti-resolver/ /etc/resolver/, \
                 /bin/rm -rf /etc/resolver/
